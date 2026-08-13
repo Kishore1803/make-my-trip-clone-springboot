68d3963f468bbb1311ec40d0311d36df81c3fb68
Kishore K <kannan.v4704@gmail.com>
2026-08-13 20:43:30 +0530

    application properties update

--- makemytrip/src/main/java/com/makemytrip/makemytrip/repository/FlightRepository.java
+++ makemytrip/src/main/java/com/makemytrip/makemytrip/repository/FlightRepository.java
@@ -1,6 +1,5 @@
 package com.makemytrip.makemytrip.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.makemytrip.makemytrip.models.Flight;
 
