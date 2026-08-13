811d13cd9c0de81880d10a6505c3411c6331485c
Kishore K <kannan.v4704@gmail.com>
2026-08-13 20:53:09 +0530

    API Url  Update

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8081";
+const BACKEND_URL = "https://make-my-trip-clone-springboot-a5az.onrender.com";
 
 const API = axios.create({
   baseURL: BACKEND_URL,
