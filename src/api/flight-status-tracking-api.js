4375c5ec387ed553d4bc93922f79dd9dbc52aa6e
Kishore K <kannan.v4704@gmail.com>
2026-09-07 11:50:55 +0530

    Update the Makemytrip-makemytour

--- src/api/flight-status-tracking-api.js
+++ src/api/flight-status-tracking-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://make-my-trip-clone-springboot-Backend.onrender.com";
+const API_URL = "https://make-my-trip-clone-springboot-htyo.onrender.com";
 
 // Get all flight statuses
 export const getFlightStatus = async () => {
